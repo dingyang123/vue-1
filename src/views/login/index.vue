abe50c38b5e1be43ca0f5adf784fc3c6915b13df
dingyang123 <1245748805@qq.com>
2019-08-09 09:51:01 +0800

    自定义信息

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,38 +4,57 @@
     <van-nav-bar title="黑马头条" />
     <!-- 输入框 -->
     <van-cell-group>
-  <van-field
-    v-validate="'required|digits:11'"
-    name="mobile"
-    v-model="user.mobile"
-    label="手机号"
-    placeholder="请输入手机号"
-    clearable
-    left-icon='contact'
-    :error-message="errors.first('mobile')"
-  />
-  <van-field
-    v-model="user.code"
-    label="验证码"
-    placeholder="请输入验证码"
-     clearable
-     left-icon='like-o'
-  >
-  <van-button slot="button" size="small" type="default">发送验证码</van-button>
-  </van-field>
-</van-cell-group>
+      <van-field
+        v-validate="'required|digits:11'"
+        name="mobile"
+        v-model="user.mobile"
+        label="手机号"
+        placeholder="请输入手机号"
+        clearable
+        left-icon="contact"
+        :error-message="errors.first('mobile')"
+      />
+      <van-field
+        v-validate="'required|digits:6'"
+        name="code"
+        v-model="user.code"
+        label="验证码"
+        placeholder="请输入验证码"
+        clearable
+        left-icon="like-o"
+        :error-message="errors.first('code')"
+      >
+        <van-button slot="button" size="small" type="default">发送验证码</van-button>
+      </van-field>
+    </van-cell-group>
 
-<!-- 登录按钮 -->
-<div class="login-btn">
-    <van-button @click="handleLogin" class="btn" type="info">登录</van-button>
-</div>
-
+    <!-- 登录按钮 -->
+    <div class="login-btn">
+      <van-button @click="handleLogin" class="btn" type="info">登录</van-button>
+    </div>
   </div>
 </template>
 
 <script>
 import { login } from '@/api/user.js'
 export default {
+  created () {
+    const dict = {
+      custom: {
+        mobile: {
+          required: '手机号不能为空',
+          digits: '手机号必须为11位'
+        },
+        code: {
+          required: '验证码不能为空',
+          digits: '验证码必须为6位'
+        }
+      }
+    }
+
+    // or use the instance method
+    this.$validator.localize('zh_cn', dict)
+  },
   data () {
     return {
       user: {
@@ -45,8 +64,13 @@ export default {
     }
   },
   methods: {
-    async   handleLogin () {
+    async handleLogin () {
       // 表单验证
+      const valid = await this.$validator.validate()
+      if (!valid) {
+        // do stuff if not valid.
+        return this.$toast.fail('登录失败')
+      }
       try {
         const data = await login(this.user)
         // 记录登录状态
@@ -54,6 +78,7 @@ export default {
         this.$store.commit('setUser', data)
         // 跳转页面
         this.$router.push({ name: 'home' })
+        this.$toast.success('登录成功')
       } catch (err) {
         this.$message.error('登录失败' + err)
       }
@@ -63,13 +88,12 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.login-btn{
-    padding: 20px;
-    display: flex;
-    justify-content: center;
-    .btn{
-        width: 60%;
-
-    }
+.login-btn {
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  .btn {
+    width: 60%;
+  }
 }
 </style>
