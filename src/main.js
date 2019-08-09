aa6c2baaf48051a63241e51bac1bd01a22685b2a
dingyang123 <1245748805@qq.com>
2019-08-09 09:23:40 +0800

    验证规则

--- src/main.js
+++ src/main.js
@@ -4,10 +4,19 @@ import router from './router'
 import store from './store'
 import Vant from 'vant'
 import 'vant/lib/index.css'
+
 // 设置rem基准
 import 'amfe-flexible'
+
+import '@/styles/index.less'
+import VeeValidate, { Validator } from 'vee-validate'
+// 导入中文文件
+import zhCN from 'vee-validate/dist/locale/ja'
 Vue.config.productionTip = false
 Vue.use(Vant)
+Vue.use(VeeValidate)
+// 配置语言
+Validator.localize('zh_cn', zhCN)
 new Vue({
   router,
   store,
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,15 +1,75 @@
 <template>
-    <div class="login-wrap">
-        zhuceyem
-    </div>
+  <div class="login-wrap">
+    <!-- 导航头 -->
+    <van-nav-bar title="黑马头条" />
+    <!-- 输入框 -->
+    <van-cell-group>
+  <van-field
+    v-validate="'required|digits:11'"
+    name="mobile"
+    v-model="user.mobile"
+    label="手机号"
+    placeholder="请输入手机号"
+    clearable
+    left-icon='contact'
+    :error-message="errors.first('mobile')"
+  />
+  <van-field
+    v-model="user.code"
+    label="验证码"
+    placeholder="请输入验证码"
+     clearable
+     left-icon='like-o'
+  >
+  <van-button slot="button" size="small" type="default">发送验证码</van-button>
+  </van-field>
+</van-cell-group>
+
+<!-- 登录按钮 -->
+<div class="login-btn">
+    <van-button @click="handleLogin" class="btn" type="info">登录</van-button>
+</div>
+
+  </div>
 </template>
 
 <script>
+import { login } from '@/api/user.js'
 export default {
-
+  data () {
+    return {
+      user: {
+        mobile: '13911111111',
+        code: '246810'
+      }
+    }
+  },
+  methods: {
+    async   handleLogin () {
+      // 表单验证
+      try {
+        const data = await login(this.user)
+        // 记录登录状态
+        // window.localStorage.setItem('user', data)
+        this.$store.commit('setUser', data)
+        // 跳转页面
+        this.$router.push({ name: 'home' })
+      } catch (err) {
+        this.$message.error('登录失败' + err)
+      }
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
+.login-btn{
+    padding: 20px;
+    display: flex;
+    justify-content: center;
+    .btn{
+        width: 60%;
 
+    }
+}
 </style>
